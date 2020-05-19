6bc41a2f95cb864ea0a4fab2f72a03c7152abb46
igorskyflyer <igor.dvlpr@gmail.com>
2020-05-19 22:02:23 +0200

    Add tests for getAudioBitrate()

--- test/mp3size.test.js
+++ test/mp3size.test.js
@@ -1,6 +1,19 @@
 const mp3 = require('../lib/mp3size')
 const chai = require('chai').assert
 
+/** Since MP3 files are loosly compressed files and can have a variable bitrate, a function of allowed value deviation is used.
+ * @param {number} value
+ * @param {number} offset
+ * @param {number} expect
+ * @returns {boolean}
+ */
+
+function deviation(value, offset, expect) {
+  return value >= expect - offset && value <= expect + offset
+}
+
+// the tests up ahead consist of real data of audio files stored on my computer
+
 describe('getFileSize()', () => {
   it('Should return -1', () => {
     chai.equal(mp3.getFileSize(''), -1)
@@ -19,6 +32,30 @@ describe('getFileSize()', () => {
   })
 
   it('Should return 4464', () => {
-    chai.equal(mp3.getFileSize('03:6', 192), 4464)
+    chai.equal(mp3.getFileSize('03:06', 192), 4464)
+  })
+})
+
+describe('getAudioBitrate()', () => {
+  it('Should return -1', () => {
+    chai.equal(mp3.getAudioBitrate(''), -1)
+  })
+
+  it('Should return ~192', () => {
+    const bitrate = mp3.getAudioBitrate('03:42', 5346)
+
+    chai.equal(deviation(bitrate, 3, 192), true)
+  })
+
+  it('Should return ~160', () => {
+    const bitrate = mp3.getAudioBitrate('01:12:14', 86680)
+
+    chai.equal(deviation(bitrate, 3, 160), true)
+  })
+
+  it('Should return ~320', () => {
+    const bitrate = mp3.getAudioBitrate('02:47', 6715)
+
+    chai.equal(deviation(bitrate, 3, 320), true)
   })
 })
