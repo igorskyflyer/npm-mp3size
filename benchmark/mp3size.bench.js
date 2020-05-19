15f7593f7d454dca921ed07d4895f4c49b8f0f15
igorskyflyer <igor.dvlpr@gmail.com>
2020-05-19 22:12:20 +0200

    Add benchmark tests for getAudioBitrate()

--- benchmark/mp3size.bench.js
+++ benchmark/mp3size.bench.js
@@ -28,10 +28,34 @@ suite.add('mp3.getFileSize("01:12:14", 320)', {
   },
 })
 
+suite.add('mp3.getFileSize("03:06", 192)', {
+  fn: () => {
+    mp3.getFileSize('03:06', 192)
+  },
+})
+
+suite.add('mp3.getAudioBitrate("")', {
+  fn: () => {
+    mp3.getAudioBitrate('')
+  },
+})
+
+suite.add('mp3.getAudioBitrate("03:42", 5346)', {
+  fn: () => {
+    mp3.getAudioBitrate('03:42', 5346)
+  },
+})
+
+suite.add('mp3.getAudioBitrate("01:12:14", 86680', {
+  fn: () => {
+    mp3.getAudioBitrate('01:12:14', 86680)
+  },
+})
+
 suite
-  .add('mp3.getFileSize("03:6", 192)', {
+  .add('mp3.getAudioBitrate("02:47", 6715)', {
     fn: () => {
-      mp3.getFileSize('03:6', 192)
+      mp3.getAudioBitrate('02:47', 6715)
     },
   })
   .on('cycle', (event) => {
